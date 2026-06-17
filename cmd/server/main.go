9130b563eff4259c8cd3ba4e0b81c90a7ccc4022
Antriksh Gwal <antrikshgwal@gmail.com>
2026-06-18 00:38:48 +0530

    Implement WS broadcast hub

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,50 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+type Hub struct {
+register   chan *websocket.Conn
+unregister chan *websocket.Conn
+
+broadcast chan []byte
+
+clients map[*websocket.Conn]bool
+}
+
+func  newHub() *Hub {
+	return &Hub{
+		register:   make(chan *websocket.Conn),
+		unregister: make(chan *websocket.Conn),
+		broadcast:  make(chan []byte),
+		clients:    make(map[*websocket.Conn]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for{
+		select {
+		case registeredConn := <-h.register:
+		h.clients[registeredConn] = true
+		log.Printf("Client registered: %v", registeredConn.RemoteAddr())
+
+		case unregisteredConn := <-h.unregister:
+		if _, ok := h.clients[unregisteredConn]; ok {
+			delete(h.clients, unregisteredConn)
+			log.Printf("Client unregistered: %v", unregisteredConn.RemoteAddr())
+		}
+
+		case message := <-h.broadcast:
+		for conn := range h.clients {
+			if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Printf("Failed to send message to client: %v", err)
+				conn.Close()
+				delete(h.clients, conn)
+			}
+		}
+	}
+	}
 }
 
 func main() {
@@ -37,5 +81,11 @@ func main() {
 		}
 	})
 
-	http.ListenAndServe(":8080", mux)
+	h := newHub()
+	go h.run()
+
+	log.Println("WebSocket server listening on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
 }
